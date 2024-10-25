24c41802ebd5c55fcf46572e10ff2e81f3873f3e
tetrisface <89279737+tetrisface@users.noreply.github.com>
2024-10-25 08:32:46 +0200

    app test

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tetrisface/NuttyB-Raptors',
+  base: '/NuttyB-Raptors',
   plugins: [vue()],
   build: {
     outDir: '../docs',
